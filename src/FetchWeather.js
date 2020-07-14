a5909e42501e9630fe5e8ca703f9ba84a46ddb01
Karl C <karl.cc14@gmail.com>
2020-07-14 15:17:19 +1200

    ui finished
    completed most of ui except search bar:
    -2 more background colours
    -minimal description
    -added max/min temps
    -added wind speed

--- src/FetchWeather.js
+++ src/FetchWeather.js
@@ -1,16 +1,22 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
-import './FetchWeather.css';
+import './FetchWeather.css'
+// import './wind.png'
 
 const appid = process.env.REACT_APP_API_KEY;
 
 function DataFetching() {
     const [city_name, setCity_name] = useState()
+    const [country, setCountry] = useState()
     const [icon, setIcon] = useState()
     let icon_path = `http://openweathermap.org/img/wn/${icon}@4x.png`
     const [short_desc, setShort_desc] = useState()
     const [long_desc, setLong_desc] = useState()
+    const [weather_id, setWeather_id] = useState()
     const [temp, setTemp] = useState()
+    const [temp_min, setTemp_min] = useState()
+    const [temp_max, setTemp_max] = useState()
+    const [wind_speed, setWind_speed] = useState()
     const [clouds, setClouds] = useState()
     const [user_input, setInput] = useState('Auckland')
     const [new_input, setNew_input] = useState('Auckland')
@@ -25,15 +31,38 @@ function DataFetching() {
         }
       }
 
+    const setBackground = () => {
+        if (short_desc === "Clear") {
+            document.body.style.backgroundColor = "azure";
+         }
+        else if (short_desc === "Rain") {
+            if (weather_id === 500 || weather_id === 501 || weather_id === 520 || weather_id === 521) {
+                document.body.style.backgroundColor = "skyblue";
+            }
+            else {document.body.style.backgroundColor = "steelblue";}
+        }
+        else if (short_desc === "Clouds") {
+            if(weather_id === 801 || weather_id == 802){
+                document.body.style.backgroundColor = "gainsboro";
+            }
+            else {document.body.style.backgroundColor = "silver";}        
+        }
+    }
+
     useEffect(() => {
         axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${new_input}&appid=${appid}`)
             .then(res => {
                 console.log(res.data)
                 setCity_name(res.data.name)
+                setCountry(res.data.sys.country)
                 setIcon(res.data.weather[0].icon)
                 setShort_desc(res.data.weather[0].main)
                 setLong_desc(res.data.weather[0].description)
+                setWeather_id(res.data.weather[0].id)
                 setTemp(res.data.main.temp)
+                setTemp_min(res.data.main.temp_min)
+                setTemp_max(res.data.main.temp_max)
+                setWind_speed(res.data.wind.speed)
                 setClouds(res.data.clouds.all)
             })
             .catch(err => {
@@ -41,22 +70,7 @@ function DataFetching() {
             })
     }, [new_input])
 
-    // console.log(city_name);
-    // console.log(desc);
-    // console.log(temp);
-    // console.log(clouds);
-
-    // document.getElementById('icon').src = `http://openweathermap.org/img/wn/${icon}@4x.png`;
-
-    if (short_desc === "Clear") {
-        document.body.style.backgroundColor = "azure";
-     }
-    else if (short_desc === "Rain") {
-        document.body.style.backgroundColor = "skyblue";
-    }
-    else if (short_desc === "Clouds") {
-        document.body.style.backgroundColor = "gainsboro";
-    }
+    setBackground()
 
     return (  
         <div>
@@ -66,12 +80,17 @@ function DataFetching() {
                 onKeyPress={e => handleKeyPress(e)}
                 />
             <button type="button" onClick={handleClick}>Submit</button>
-            <p>{city_name}</p>
+            <p>{city_name}, {country}</p>
             <img id="icon" src={icon_path} />
-            {/* <script type="text/javascript">document.getElementById('icon').src = 'http://openweathermap.org/img/wn/${icon}@4x.png';</script> */}
-            <p>{Math.round((temp - 273.15) * 10) / 10}&#176; C</p>
-            <p>{short_desc} : {long_desc}</p>
-            {/* <p>Clouds: {clouds}</p> */}
+            <div class="container">
+                <p>{Math.round((temp - 273.15) * 10) / 10}&#176;C</p>
+                <p>{Math.round((temp_max - 273.15) * 1) / 1}&frasl;{Math.round((temp_min - 273.15) * 1) / 1}&#176;</p>
+                    <div id="wind">
+                        <img id="wind-icon" src={require('./wind.png')} height="100px" />
+                        <p>{wind_speed} <span id="wind-speed">m/s</span></p>
+                    </div>
+            </div>               
+            <p>{long_desc}</p>
         </div>
     )
 }
